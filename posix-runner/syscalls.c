9d4a1e215982e02599a28402cd6173c49667ea29
Andrius Štikonas <andrius@stikonas.eu>
2023-12-29 00:15:23 +0000

    Add sys_close.

--- posix-runner/syscalls.c
+++ posix-runner/syscalls.c
@@ -22,6 +22,11 @@ int sys_open(char* name, int flag, int mode, void, void, void)
     return open(name, flag, mode);
 }
 
+int sys_close(int fd, void, void, void, void, void)
+{
+    return close(fd);
+}
+
 int sys_lseek(int fd, int offset, int whence, void, void, void)
 {
     return lseek(fd, offset, whence);
@@ -60,6 +65,7 @@ void init_syscalls()
     syscall_table[0] = sys_read;
     syscall_table[1] = sys_write;
     syscall_table[2] = sys_open;
+    syscall_table[3] = sys_close;
     syscall_table[8] = sys_lseek;
     syscall_table[12] = sys_brk;
     syscall_table[60] = sys_exit;
